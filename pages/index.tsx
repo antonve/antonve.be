f67ae492dde27966f4337864c3d038c9d0c15161
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-08-25 21:39:53 +0900

    change index to redirect to public resume

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
-import styled from 'styled-components'
+import React from 'react'
+import Router from 'next/router'
 
-const Home = () => <Wrapper>Welcome to Next.js!</Wrapper>
+const defaultRoute = '/public'
 
-export default Home
-
-const Wrapper = styled.h1`
-  color: blue;
-`
+export default class extends React.Component {
+  static async getInitialProps({ res }) {
+    if (res) {
+      res.writeHead(302, {
+        Location: defaultRoute,
+      })
+      res.end()
+    } else {
+      Router.push(defaultRoute)
+    }
+    return {}
+  }
+}
