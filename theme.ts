f020824eec462344bf3d4b36a3b2bb0bd4549eef
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-08-26 22:20:21 +0900

    add theme

---
+++ theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    light: `#fff`,
+    light2: `#fcfafa`,
+    dark: `#000`,
+    accent: `#2996EE`,
+  },
+  screenSizes: {
+    mobileLarge: 784,
+    mobileMiddle: 600,
+    mobileSmall: 525,
+    mobileMicro: 400,
+  },
+  baseFontSize: 62.5,
+}
