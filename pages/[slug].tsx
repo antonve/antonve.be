29465f78b8279e386160345afedbb3267201da44
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-08-26 22:21:57 +0900

    add wrapper to resume page

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -3,14 +3,18 @@ import fetch from 'isomorphic-unfetch'
 
 import constants from '../constants'
 import { Resume } from '../domain'
+import styled from 'styled-components'
 
 interface Props {
   resume: Resume
 }
 
-const ResumePage = ({ resume }: Props) => {
-  return <p>slug: {resume.skills[0].label}</p>
-}
+const ResumePage = ({ resume }: Props) => (
+  <Wrapper>
+    personal
+    <Separator />
+  </Wrapper>
+)
 
 ResumePage.getInitialProps = async ({ query }: NextPageContext) => {
   const url = `${constants.apiRoot}/resume/${query.slug}`
@@ -21,3 +25,17 @@ ResumePage.getInitialProps = async ({ query }: NextPageContext) => {
 }
 
 export default ResumePage
+
+const Wrapper = styled.div`
+  max-width: 784px;
+  box-sizing: border-box;
+  padding: 60px 20px;
+`
+
+const Separator = styled.hr`
+  margin: 50px auto;
+  max-width: 500px;
+  height: 1px;
+  border: 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.05);
+`
