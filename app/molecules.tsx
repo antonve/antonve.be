ebc8136e4a07954c32b19b5fb1f08a12fc556c9f
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-08-30 22:16:54 +0900

    add molecules

---
+++ app/molecules.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import { H1, Link } from './atoms'
+
+const Introduction = () => {
+  const email = 'mail@antonve.be'
+
+  return (
+    <Wrapper>
+      <H1>Anton Van Eechaute</H1>
+      <Body>
+        Tokyo, Japan
+        <br />
+        <Link href={`mailto:${email}`}>{email}</Link>
+      </Body>
+    </Wrapper>
+  )
+}
+
+export { Introduction }
+
+const Wrapper = styled.div`
+  text-align: center;
+`
+
+const Body = styled.p`
+  font-size: 2.8em;
+  line-height: 1.4em;
+  margin: 0;
+  padding: 0;
+`
