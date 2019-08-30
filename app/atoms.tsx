c274a0b1bb3a0f39093f42d2a0b0e596d8e75715
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-08-30 22:16:43 +0900

    add atoms

---
+++ app/atoms.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  font-size: 3.4em;
+  font-weight: bold;
+  margin: 0 0 20px 0;
+`
+
+const H2 = styled.h1`
+  font-size: 2.8em;
+  font-weight: bold;
+  margin: 0 0 20px 0;
+`
+
+const Link = styled.a`
+  color: ${props => props.theme.colors.dark};
+  text-decoration: none;
+  position: relative;
+  padding-bottom: 2px;
+  text-shadow: 1px 1px #fff, -1px 1px #fff, -1px -1px #fff, 1px -1px #fff;
+  z-index: 1;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    height: 2px;
+    bottom: 4px;
+    left: 0;
+    width: 100%;
+    opacity: 0.05;
+    background: ${props => props.theme.colors.dark};
+  }
+
+  &:after {
+    background: ${props => props.theme.colors.accent};
+    content: '';
+    position: absolute;
+    z-index: -1;
+    width: 100%;
+    transform: scaleX(0);
+    height: 2px;
+    bottom: 4px;
+    left: 0;
+    transform-origin: bottom right;
+    transition: transform 0.25s ease-out;
+  }
+
+  &:hover:after,
+  &.active:after,
+  &:focus:after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+  }
+`
+
+export { H1, H2, Link }
