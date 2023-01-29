6de03a4454610955a1d2cc46545b2f31a6dea461
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-29 20:32:32 +0900

    configure base styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF225A',
+        secondary: '#18D6CC',
+      },
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        serif: ['Merriweather', ...defaultTheme.fontFamily.serif]
+      },
+      minWidth: (theme) => ({
+        ...theme('spacing'),
+      }),
+      minHeight: (theme) => ({
+        ...theme('spacing'),
+      }),
+    },
   },
   plugins: [],
 }
