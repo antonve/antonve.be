67e2645f259cdc490a0d9c72e8f73c32c11a469b
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-02-19 21:51:23 +0900

    fix api endpoint for blog posts

--- app/api.ts
+++ app/api.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 import { useQuery } from 'react-query'
 
-const root = `https://staging.tadoku.app/api/internal/content`
+const root = `https://tadoku.app/api/internal/content`
 const namespace = 'antonve'
 
 const Post = z.object({
